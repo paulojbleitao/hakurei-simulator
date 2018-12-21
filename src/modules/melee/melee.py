36d7d20bbf776ad7af791121101ea8c1c94d717d
paulojbleitao <paulojbleitao@gmail.com>
2018-12-20 21:00:09 -0300

    Fix a little logic mistake

--- src/modules/melee/melee.py
+++ src/modules/melee/melee.py
@@ -5,5 +5,5 @@ class Melee():
     async def on_message(self, message):
         if 'melee' in message.content.lower() and not self.talked:
             self.talked = True
-            msg = 'bora' if message.content.lower() == 'bora melee' else 'bora melee'
+            msg = 'bora' if 'bora melee' in message.content.lower() else 'bora melee'
             await message.channel.send(msg)
