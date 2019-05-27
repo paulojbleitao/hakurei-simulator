48b1332223d04a96c40806026dd157663220406b
paulojbleitao <paulojbleitao@gmail.com>
2019-05-27 13:21:46 -0300

    Reduce melee messages frequency

--- src/modules/melee/melee.py
+++ src/modules/melee/melee.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 class Melee():
     def __init__(self):
         self.talked = False
-    
+        self.date = datetime.today().weekday() 
+
     async def on_message(self, message):
-        if 'melee' in message.content.lower() and not self.talked:
+        if 'melee' in message.content.lower() and not self.talked and self.date == 4:
             self.talked = True
             msg = 'bora' if 'bora melee' in message.content.lower() else 'bora melee'
             await message.channel.send(msg)
